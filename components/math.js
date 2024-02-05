2a6c8be225ab9114c99bcfcaf6f6f7257ca667b3
hagenchung2 <hagen.chung@edu.sait.ca>
2024-02-05 13:48:56 -0700

    removed extra 0s

---
+++ components/math.js
@@ -0,0 +1,56 @@
+//variables to hold units, change the defaults for these based on page
+fromUnit = "kg";
+toUnit = "lb.";
+
+
+numEntry = /*PUT A FUCKING THING TO HOOK UP TO PROMPT INSTEAD OF THESE DEFAULT VALUES*/"2, 3, 5, 23".split(", ")
+
+//add fuction to button/form so it does something, idk if we wanted to use this but its here
+// function unitSwap() {
+//     switch(fromUnit){
+//         case "kg":
+//             fromUnit = toUnit;
+//             toUnit = "kg";
+//             break;
+//         case "lb.":
+//             fromUnit = toUnit;
+//             toUnit = "lb.";
+//         break;
+//     }
+// }
+
+
+
+//take in 2 parameters (unit being converted from + unit being converted to)
+function createNewConverter (a = fromUnit, b = toUnit) {
+    //return function that takes number parameter
+    //
+    switch(a, b){
+        case "°F", "°C":
+            return conversion = convNum => {(convNum - 32) * 5 / 9;}
+            break;
+        case "°C", "°F":
+            return conversion = convNum => (convNum * 9 / 5) + 32;
+            break;
+        case "kg", "lb.":
+            return conversion = convNum => convNum * 2.20462262185;
+            break;
+        case "lb.", "kg":
+            return conversion = convNum => convNum / 2.20462262185;
+            break;
+        case "km", "Miles":
+            return conversion = convNum => convNum * 0.621371;
+            break;
+        case "Miles", "km":
+            return conversion = convNum => convNum / 0.621371;
+            break;
+    }
+}
+
+
+
+newArray = numEntry.map(createNewConverter());
+
+//these are for testing input+returned values
+console.log(numEntry)
+console.log(newArray)
