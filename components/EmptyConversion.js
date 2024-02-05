1a62d6cb60e54f6dde74820a9a6e5739280d98ca
ricky <39970880+torickyto@users.noreply.github.com>
2024-02-04 18:57:29 -0700

    added contrasting boxes and colors

--- components/EmptyConversion.js
+++ components/EmptyConversion.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const EmptyConversion = () => {
   return (
-    <p className="text-2xl font-bold text-gray-100 opacity-80 text-center">
+    <p className="text-2xl font-bold text-neutral-200 text-center">
       Select conversion below
     </p>
   );
--- components/MainUI.js
+++ components/MainUI.js
@@ -4,11 +4,11 @@ import EmptyConversion from './EmptyConversion';
 const MainUI = () => {
   return (
     <div className="relative flex flex-col items-center justify-between h-screen p-14">
-      <h1 className="text-4xl lg:text-6xl font-extrabold bg-gradient-to-r from-gray-600 to-gray-800 text-transparent bg-clip-text">
+      <h1 className="text-4xl lg:text-6xl font-extrabold bg-gradient-to-r from-neutral-700 to-neutral-900 text-transparent bg-clip-text border-b-4 border-gray-600 rounded-md">
         Converto
       </h1>
 
-      <div className="relative p-14 mb-22 lg:w-full lg:max-w-7xl z-0 border border-gray-300 bg-neutral-800 rounded-xl mt-[-100px]">
+      <div className="relative p-14 mb-22 lg:w-full lg:max-w-7xl z-0 border border-neutral-300 bg-neutral-800 rounded-xl mt-[-100px]">
       <EmptyConversion />
       </div>
 
@@ -17,17 +17,17 @@ const MainUI = () => {
         {/* Pounds to Kilograms */}
         <a
           href=""
-          className="group rounded-lg border border-transparent p-4 lg:p-6 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 flex flex-col items-center justify-center"
+          className="group rounded-lg border border-transparent p-4 lg:p-6 transition-colors hover:border-gray-300 hover:bg-neutral-800 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 flex flex-col items-center justify-center"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
+          <h2 className={`mb-3 text-xl font-bold group-hover:text-neutral-200`}>
             Pounds to Kilograms{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 text-sm opacity-50`}>
+          <p className={`m-0 text-sm opacity-50 group-hover:text-neutral-200`}>
             Convert pounds (lb.) to kilograms (kg) and vice versa.
           </p>
         </a>
@@ -35,17 +35,17 @@ const MainUI = () => {
         {/* Miles to Kilometres */}
         <a
           href=""
-          className="group rounded-lg border border-transparent p-4 lg:p-6 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 flex flex-col items-center justify-center"
+          className="group rounded-lg border border-transparent p-4 lg:p-6 transition-colors hover:border-gray-300 hover:bg-neutral-800 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 flex flex-col items-center justify-center"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
+          <h2 className={`mb-3 text-xl font-bold group-hover:text-neutral-200`}>
             Miles to Kilometres{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 text-sm opacity-50`}>
+          <p className={`m-0 text-sm opacity-50 group-hover:text-neutral-200`}>
             Convert miles to kilometres (km) and vice versa.
           </p>
         </a>
@@ -53,17 +53,17 @@ const MainUI = () => {
         {/* Celsius to Fahrenheit */}
         <a
           href=""
-          className="group rounded-lg border border-transparent p-4 lg:p-6 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 flex flex-col items-center justify-center"
+          className="group rounded-lg border border-transparent p-4 lg:p-6 transition-colors hover:border-gray-300 hover:bg-neutral-800 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 flex flex-col items-center justify-center"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
+          <h2 className={`mb-3 text-xl font-bold group-hover:text-neutral-200`}>
             Celsius to Fahrenheit{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 text-sm opacity-50`}>
+          <p className={`m-0 text-sm opacity-50 group-hover:text-neutral-200`}>
             Convert Celsius (°C) to Fahrenheit (°F) and vice versa.
           </p>
         </a>
